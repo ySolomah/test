ff0f2e5b1b2a24c296b05cfccd1f81dc93b763b6
Yassir Solomah <yassir.solomah@gmail.com>
2016-10-16 10:53:11 -0400

    Initial Version

---
+++ phoneWord/EmptyClass.cs
@@ -0,0 +1,88 @@
+﻿using System.Text;
+using System;
+
+namespace Core
+{
+	public static class PhonewordTranslator
+	{
+		public static string ToNumber(string raw)
+		{
+			if (raw.Length == 0)
+			{
+				return ("");
+			}
+			else
+			{
+				raw = raw.ToUpperInvariant();
+				raw = raw.Trim();
+			}
+			var newNumber = new StringBuilder();
+			foreach (char c in raw)
+			{
+				if (" -0123456789".Contains(Char.ToString(c)))
+				{
+					newNumber.Append(c);
+				}
+				else
+				{
+					var whatAmI = Translator(c);
+					if (whatAmI != null)
+					{
+						newNumber.Append(whatAmI);
+					}
+				}
+			}
+			return (newNumber.ToString());
+		}
+		public static int? Translator(char characterToBeTranslated)
+		{
+			if (inRangeOf(Convert.ToInt32(characterToBeTranslated), 65, 67))
+			{
+				return (2);
+			}
+			if (inRangeOf(Convert.ToInt32(characterToBeTranslated), 68, 70))
+			{
+				return (3);
+			}
+			if (inRangeOf(Convert.ToInt32(characterToBeTranslated), 71, 73))
+			{
+				return (4);
+			}
+			if (inRangeOf(Convert.ToInt32(characterToBeTranslated), 74, 76))
+			{
+				return (5);
+			}
+			if (inRangeOf(Convert.ToInt32(characterToBeTranslated), 77, 79))
+			{
+				return (6);
+			}
+			if (inRangeOf(Convert.ToInt32(characterToBeTranslated), 80, 83))
+			{
+				return (7);
+			}
+			if (inRangeOf(Convert.ToInt32(characterToBeTranslated), 84, 86))
+			{
+				return (8);
+			}
+			if (inRangeOf(Convert.ToInt32(characterToBeTranslated), 87, 90))
+			{
+				return (9);
+			}
+			System.Console.WriteLine("Nothing to be printed");
+			return (null);
+
+		}
+		public static bool inRangeOf(int characterToBeTranslated, int lowerBound, int higherBound)
+		{
+			if (characterToBeTranslated >= lowerBound && characterToBeTranslated <= higherBound)
+			{
+				return (true);
+			}
+			else
+			{
+				return (false);
+			}
+
+		}
+	}
+}
---
+++ phoneWord/MainActivity.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Widget;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+
+namespace phoneWord
+{
+	[Activity(Label = "phoneWord", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Activity
+	{
+		int count = 1;
+
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			// Set our view from the "main" layout resource
+			SetContentView(Resource.Layout.Main);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			//Button button = FindViewById<Button>(Resource.Id.myButton);
+
+			//button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+			EditText phoneNumberText = FindViewById<EditText>(Resource.Id.PhoneNumberText);
+			Button translateButton = FindViewById<Button>(Resource.Id.TranslateButton);
+			Button callButton = FindViewById<Button>(Resource.Id.CallButton);
+
+
+			callButton.Enabled = false;
+			string translatedNumber = string.Empty;
+
+			translateButton.Click += (object sender, System.EventArgs e) =>
+			{
+				translatedNumber = Core.PhonewordTranslator.ToNumber(phoneNumberText.Text);
+				if (translatedNumber.Length > 14 || string.IsNullOrWhiteSpace(translatedNumber) || translatedNumber.Length == 0)
+				{
+					callButton.Text = ("ascii value of B is: " + Convert.ToString(Convert.ToInt32(Convert.ToChar("B"))));
+					callButton.Enabled = false;
+				}
+				else
+				{
+					callButton.Text = ("Do you wish to call :" + translatedNumber + "?");
+					callButton.Enabled = true;
+				}
+			};
+
+			callButton.Click += (object sender, System.EventArgs e) =>
+			{
+				callButton.Text = ("Now completing the call...");
+				callButton.Enabled = false;
+				var callDialog = new AlertDialog.Builder(this);
+				callDialog.SetMessage("Call" + translatedNumber + "?");
+				callDialog.SetNeutralButton("Call", delegate {
+					var callIntent = new Intent(Intent.ActionCall);
+					callIntent.SetData(Android.Net.Uri.Parse("tel:" + translatedNumber));
+					StartActivity(callIntent);
+			    });
+				callDialog.SetNegativeButton("Cancel?", delegate { });
+
+				callDialog.Show();
+					
+			};
+		}
+	}
+}
+
